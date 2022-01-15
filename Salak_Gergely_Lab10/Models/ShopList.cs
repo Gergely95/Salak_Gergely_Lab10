45d94746749ad9f6129bd674aa9292005193bb23
Salak Gergely <92632380+Gergely95@users.noreply.github.com>
2022-01-15 15:56:31 +0200

    Add project files.

---
+++ Salak_Gergely_Lab10/Models/ShopList.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using SQLite;
+
+namespace Salak_Gergely_Lab10.Models
+{
+    public class ShopList
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+        public string Description { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
